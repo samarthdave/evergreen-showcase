fca7e52e8dfede035a472612ba3585ea3c76e51b
Samarth <samarthdave@users.noreply.github.com>
2018-12-16 20:17:49 -0600

    Build project and change config in package

---
+++ showcase/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Welcome to the Evergreen showcase.</h1>
+        <h3>Still working on it...</h3>
+      </>      
+    );
+  }
+}
+
+export default App;
